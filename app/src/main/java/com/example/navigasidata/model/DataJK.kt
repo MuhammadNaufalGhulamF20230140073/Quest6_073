c667605ff61dd7767be09da3cd230fdda693d9c7
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-21 19:33:40 +0700

    membuatobjek data jk untuk jenis kelamin

---
+++ app/src/main/java/com/example/navigasidata/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.navigasidata.model
+
+import com.example.navigasidata.R
+
+object DataJK {
+    val JenisK= listOf(
+        R.string.laki,
+        R.string.perempuan
+    )
+}
