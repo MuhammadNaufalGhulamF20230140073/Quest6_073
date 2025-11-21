8e1496e6da45c7b3577394aafa4718d725566d88
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-21 19:48:23 +0700

    menambahkan enum Navigasi dan setup awal struktur navigasi Compose

---
+++ app/src/main/java/com/example/navigasidata/view/uicontroller/Navigasi.kt
@@ -0,0 +1,17 @@
+package com.example.navigasidata.view.uicontroller
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulir,
