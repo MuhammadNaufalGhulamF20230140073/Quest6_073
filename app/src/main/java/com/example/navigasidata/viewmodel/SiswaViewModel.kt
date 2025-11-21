f96bd158f48bb9983edb70c5457bff6d7c28d16a
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-21 19:45:33 +0700

    menambahkan SiswaViewModel dengan StateFlow untuk menyimpan data siswa

---
+++ app/src/main/java/com/example/navigasidata/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.navigasidata.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.navigasidata.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
