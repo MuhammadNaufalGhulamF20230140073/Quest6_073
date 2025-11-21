a349732eeb82983b549c94c9f93a2122d83a99d9
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-21 19:37:57 +0700

    menambahkan input alamat, divider, dan tombol submit pada form

--- app/src/main/java/com/example/navigasidata/view/Formulir.kt
+++ app/src/main/java/com/example/navigasidata/view/Formulir.kt
@@ -99,3 +99,31 @@ fun FormIsian(
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = txtAlamat,
+                singleLine = true,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .width(250.dp),
+                label = {Text(text = "Alamat")},
+                onValueChange = {
+                    txtAlamat = it
+                },
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(1f),
+                enabled = txtAlamat.isNotEmpty(),
+                onClick = { OnSubmitBtnClick(listData) }
+            ) {
+                Text(stringResource(id=R.string.submit))
+            }
+        }
+    }
+}
